12ca13306c35f6ec80c6a83a55c1ae41c1affe9e
kamil.palka <cabeq1236@gmail.com>
2023-02-19 23:17:44 +0100

    add enums for scenes,animation, texture

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,31 +1,29 @@
 import Phaser from "phaser";
 
+import TextureKeys from "../consts/TextureKeys";
+import SceneKeys from "../consts/SceneKeys";
+import AnimationKeys from "../consts/AnimationKeys";
+
 export default class Game extends Phaser.Scene {
   constructor() {
-    super("game");
-  }
-
-  preload() {
-    this.load.image("background", "house/bg_repeat_340x640.png");
-
-    this.load.atlas(
-      "rocket-mouse",
-      "characters/rocket-mouse.png",
-      "characters/rocket-mouse.json"
-    );
+    super(SceneKeys.Game);
   }
 
   create() {
     const width = this.scale.width;
     const height = this.scale.height;
 
-    this.add.tileSprite(0, 0, width, height, "background").setOrigin(0);
+    this.add
+      .tileSprite(0, 0, width, height, TextureKeys.Background)
+      .setOrigin(0);
 
-    this.add.sprite(
-      width * 0.5,
-      height * 0.5,
-      "rocket-mouse",
-      "rocketmouse_fly01.png"
-    );
+    this.add
+      .sprite(
+        width * 0.5,
+        height * 0.5,
+        TextureKeys.RocketMouse,
+        "rocketmouse_fly01.png"
+      )
+      .play(AnimationKeys.RocketMouseRun);
   }
 }
