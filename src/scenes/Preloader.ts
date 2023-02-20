edab78fefeb67ed160fcf9d8f1f58675b8bd7c89
kamil.palka <cabeq1236@gmail.com>
2023-02-20 22:34:44 +0100

    add background assets and solve overlaping problem

--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -11,6 +11,10 @@ export default class Preloader extends Phaser.Scene {
   preload() {
     this.load.image(TextureKeys.Background, "house/bg_repeat_340x640.png");
     this.load.image(TextureKeys.MouseHole, "house/object_mousehole.png");
+    this.load.image(TextureKeys.Window1, "house/object_window1.png");
+    this.load.image(TextureKeys.Window2, "house/object_window2.png");
+    this.load.image(TextureKeys.Bookcase2, "house/object_bookcase2.png");
+    this.load.image(TextureKeys.Bookcase1, "house/object_bookcase1.png");
 
     this.load.atlas(
       TextureKeys.RocketMouse,
