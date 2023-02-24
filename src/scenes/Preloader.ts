61f42612b4d4e4b5c5d437c306f676d5b2aeaac8
kamil.palka <cabeq1236@gmail.com>
2023-02-24 15:39:56 +0100

    create sprite container add flames animation to sprits

--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -36,6 +36,18 @@ export default class Preloader extends Phaser.Scene {
       repeat: -1,
     });
 
+    this.anims.create({
+      key: AnimationKeys.RocketFlamesOn,
+      frames: this.anims.generateFrameNames(TextureKeys.RocketMouse, {
+        start: 1,
+        end: 2,
+        prefix: "flame",
+        suffix: ".png",
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
     this.scene.start(SceneKeys.Game);
   }
 }
